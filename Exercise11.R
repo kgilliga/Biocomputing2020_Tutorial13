136ba8107f5eed9f91d91093f042c000f9b7b3af
Keara G <kgilliga@nd.edu>
2020-11-13 08:26:31 -0500

    Working on the initial simulation

--- Exercise11.R
+++ Exercise11.R
@@ -1,13 +1,46 @@
 ##Generate a script that simulates growth of the two sub-populations in the tumor to equilibrium followed by drug treatment. 
 ##Input basic information
-timesteps=100
+timesteps=10000
 r=0.1
 K=1000000
 N0=100
+M0=1
 
-#50% of its growth rate in the absence of the drug and the non-mutant sub-population declines rapidly.
-#Assume in the absence of the cancer drug that the cells grow at a rate of 0.1 per day (rN = rM = 0.1) 
-#and the carrying capapcity (K) of the tumor is one million cells. 
-#The mutation of a single cell occurred early in the tumor growth and when it occurred there were 100 total cells in the tumor. 
-#Drug treatment of non-mutant cells results in a negative growth rate of -0.1.
+#vectors to store 
+NsEvents=numeric(length=timesteps)
+NsEvents[1]=N0
+MsEvents=numeric(length=timesteps)
+MsEvents[1]=M0
+#variable for changes in growth rate
+rnc=-0.1
+rmc=0.5*0.1
+#Original 
+NsEvents[t+1] <- NsEvents[t]+r*NsEvents[t]*(1-(NsEvents[t]+MsEvents[t])/K)
+MsEvents[t+1]<- MsEvents[t]+r*MsEvents[t]*(1-(NsEvents[t]+MsEvents[t])/K)
+
+#plot:
+normal<-data.frame(time=1:timesteps,N=NsEvents)
+library(ggplot2)
+ggplot(data=normal,aes(x=time,y=N))+geom_line()+theme_classic()
+#This would help me find the equilibrium, to know when to introduce the drug
+
+#Simulation with new events- normal subpopulation
+for(t in 1:(timesteps-1)){
+  if(t==50){
+    NsEvents[t+1] <- NsEvents[t]+rnc*NsEvents[t]*(1-(NsEvents[t]+MsEvents[t])/K)
+  }
+  else{
+    NsEvents[t+1] <- NsEvents[t]+r*NsEvents[t]*(1-(NsEvents[t]+MsEvents[t])/K)
+  }
+}
+#Simulation with new events-Mutant subpopulation
+for(t in 1:(timesteps-1)){
+  if(t==50){
+    MsEvents[t+1] <- MsEvents[t]+rmc*MsEvents[t]*(1-(NsEvents[t]+MsEvents[t])/K)
+  }
+  else{
+    MsEvents[t+1] <- MsEvents[t]+r*MsEvents[t]*(1-(NsEvents[t]+MsEvents[t])/K)
+  }
+}
 #Plot your results using a line graph.
+
